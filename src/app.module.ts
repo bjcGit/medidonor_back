b73ca56da3cd9cab9937ffb39a1ee0e049b45768
Bryan <bryan.bjc2@hotmail.com>
2025-05-24 01:56:53 -0500

    con reject

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,40 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { CommonModule } from './common/common.module';
-import { AuthModule } from './auth/auth.module';
-import { UsuariosModule } from './usuarios/usuarios.module';
-import { RolesModule } from './roles/roles.module';
-import { SeedModule } from './seed/seed.module';
-
-
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { CommonModule } from "./common/common.module";
+import { AuthModule } from "./auth/auth.module";
+import { UsuariosModule } from "./usuarios/usuarios.module";
+import { RolesModule } from "./roles/roles.module";
+import { SeedModule } from "./seed/seed.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({   
-      type: 'postgres',
-      host: process.env.PD_HOST,
-      port: +process.env.PD_PORT,
-      username: process.env.PD_USERNAME,
-      password: process.env.PD_PASSWORD,
-      database: process.env.PD_NAMEDB,
+    // TypeOrmModule.forRoot({
+    //   type: "postgres",
+    //   host: process.env.PD_HOST,
+    //   port: +process.env.PD_PORT,
+    //   username: process.env.PD_USERNAME,
+    //   password: process.env.PD_PASSWORD,
+    //   database: process.env.PD_NAMEDB,
+    //   autoLoadEntities: true,
+    //   synchronize: false,
+    // }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      url: process.env.DATABASE_URL, // Usa la URL directamente
       autoLoadEntities: true,
-      synchronize: false
+      synchronize: false,
+      ssl: {
+        rejectUnauthorized: false, // Necesario para Render
+      },
     }),
 
-    CommonModule,   
+    CommonModule,
     AuthModule,
     UsuariosModule,
     RolesModule,
-    SeedModule, 
+    SeedModule,
   ],
   controllers: [],
   providers: [],
