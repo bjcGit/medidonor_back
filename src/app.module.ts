b31226744b44188f484adbc217a35757816288e0
Luis Sebastian Cuellar Bernal <32972122+Cubasstian@users.noreply.github.com>
2025-07-05 14:51:09 -0500

    Update app.module.ts
    asd

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,26 @@
-import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { CommonModule } from "./common/common.module";
-import { AuthModule } from "./auth/auth.module";
-import { UsuariosModule } from "./usuarios/usuarios.module";
-import { SeedModule } from "./seed/seed.module";
-import { MedicamentosModule } from "./medicamentos/medicamentos.module";
-import { EntregasModule } from "./entregas/entregas.module";
+import { CommonModule } from './common/common.module';
+import { AuthModule } from './auth/auth.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { SeedModule } from './seed/seed.module';
+import { MedicamentosModule } from './medicamentos/medicamentos.module';
+import { EntregasModule } from './entregas/entregas.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-
-function getSslOption() {
-  const sslReject = process.env.SSL_REJECT_UNAUTHORIZED;
-  if (sslReject === 'false') return { rejectUnauthorized: false };
-  if (sslReject === 'true') return { rejectUnauthorized: true };
-  return false; // 🔥 clave: no dejarlo como undefined
-}
-
     TypeOrmModule.forRoot({
-  type: 'postgres',
-  url: process.env.DATABASE_URL,
-  autoLoadEntities: true,
-  synchronize: true,
-  ssl: getSslOption(),
-}),
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl: process.env.SSL_REJECT_UNAUTHORIZED === 'false'
+        ? { rejectUnauthorized: false }
+        : false,
+    }),
     CommonModule,
     AuthModule,
     UsuariosModule,
